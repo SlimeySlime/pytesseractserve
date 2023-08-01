f201e2d0373b3297d8c5b2a36e830e4158c0470b
slimeyslime <tmffkdal@gmail.com>
2023-08-01 18:43:15 +0900

    picture to cv

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png']
 IMAGE_FILE_PATH = '/img/'
 FIG_IMG_NUM = 1
 cols = 3
-rows = 3
+rows = 5
 
 def add_subplot_image(figure, src, title, img_num):
     ax1 = figure.add_subplot(rows, cols, img_num) 
@@ -66,7 +66,7 @@ def do_thing(image_link):
         adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         thresholdType=cv2.THRESH_BINARY_INV,
         blockSize=31,
-        C=5
+        C=9
     )
     image_to_show.append(img_threshed2)
 
@@ -76,11 +76,12 @@ def do_thing(image_link):
         adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
         thresholdType=cv2.THRESH_BINARY_INV,
         blockSize=11,
-        C=11
+        C=9
     )
     image_to_show.append(img_threshed3)
 
-    contours, _ = cv2.findContours(img_threshed, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+    # 테두리 검출 
+    contours, _ = cv2.findContours(img_threshed3, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
     temp_result = np.zeros((HEIGHT, WIDTH, CHANNEL), dtype=np.uint8)
     cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255,255,255))
     temp_result = np.zeros((HEIGHT, WIDTH, CHANNEL), dtype=np.uint8)
@@ -101,6 +102,147 @@ def do_thing(image_link):
     image_to_show.append(temp_result)
 
 
+    # 테두리 중 번호판 검출 
+    # MIN_AREA = 40
+    MIN_AREA = 1
+    # MIN_WIDTH, MIN_HEIGHT = 2, 8
+    MIN_WIDTH, MIN_HEIGHT = 0.2, 0.8
+    MIN_RATIO, MAX_RATIO = 0.25, 1.0
+    possible_contours = []
+    cnt = 0
+    for d in contours_dict:
+        area = d['w'] * d['h']
+        ratio = d['w'] / d['h']
+        if area > MIN_AREA and d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT and MIN_RATIO < ratio < MAX_RATIO:
+            d['idx'] = cnt
+            cnt += 1
+            possible_contours.append(d)
+    temp_result = np.zeros((HEIGHT, WIDTH, CHANNEL), dtype=np.uint8)
+    for d in possible_contours:
+        cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255,255,255), thickness=2)
+    image_to_show.append(temp_result)
+
+    def find_chars(contour_list):
+        MAX_DIAG_MULTIPLYER = 5
+        MAX_ANGLE_DIFF = 12.0
+        MAX_AREA_DIFF = 0.5
+        MAX_WIDTH_DIFF = 0.8
+        MAX_HEIGHT_DIFF = 0.2
+        MIN_N_MATCHED = 3
+
+        matched_result_idx = []
+
+        for d1 in contour_list:
+
+            matched_contours_idx = []
+
+            for d2 in contour_list:
+                if d1['idx'] == d2['idx']:
+                    continue
+                dx = abs(d1['cx'] - d2['cx'])
+                dy = abs(d1['cy'] - d2['cy'])
+
+                diagonal_length = np.sqrt(d1['w'] ** 2 + d1['h'] ** 2)
+                distance = np.linalg.norm(np.array([d1['cx'], d1['cy']]) - np.array([d2['cx'], d2['cy']]))
+                if dx == 0:
+                    angle_diff = 90
+                else:
+                    angle_diff = np.degrees(np.arctan(dy / dx)) 
+                area_diff = abs(d1['w'] * d1['h'] - d2['w'] * d2['h']) / (d1['w'] * d1['h'])
+                width_diff = abs(d1['w'] - d2['w']) / d1['w']
+                height_diff = abs(d1['h'] - d2['h']) / d1['h']
+
+                if distance < diagonal_length * MAX_DIAG_MULTIPLYER \
+                and angle_diff < MAX_ANGLE_DIFF and area_diff < MAX_AREA_DIFF \
+                and width_diff < MAX_WIDTH_DIFF and height_diff < MAX_HEIGHT_DIFF:
+                    matched_contours_idx.append(d2['idx'])
+
+
+            matched_contours_idx.append(d1['idx'])
+
+            if len(matched_contours_idx) < MIN_N_MATCHED:
+                continue
+            
+            matched_result_idx.append(matched_contours_idx)
+
+            unmatched_contours_idx = []
+            for d4 in contour_list:
+                if d4['idx'] not in matched_contours_idx:
+                    unmatched_contours_idx.append(d4['idx'])
+
+            unmatched_contours_idx = np.take(possible_contours, unmatched_contours_idx)
+
+            recursive_contour_list = find_chars(unmatched_contours_idx)
+
+            for idx in recursive_contour_list:
+                matched_result_idx.append(idx)
+
+            break
+    
+        return matched_result_idx
+
+
+    result_idx = find_chars(possible_contours)
+    print(result_idx)
+    matched_result = []
+    for idx_list in result_idx:
+        matched_result.append(np.take(possible_contours, idx_list))
+    
+    temp_result = np.zeros((HEIGHT, WIDTH, CHANNEL), dtype=np.uint8)
+    for r in matched_result:
+        for d in r:
+            cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255,255,255), thickness=2)
+    image_to_show.append(temp_result)
+
+
+    PLATE_WIDTH_PADDING = 1.5
+    PLATE_HEIGHT_PADDING = 1.5
+    MIN_PLATE_RATIO = 3
+    MAX_PLATE_RATIO = 10
+
+    plate_imgs = []
+    plate_infos = []
+
+    for i, matched_chars in enumerate(matched_result):
+        sorted_chars = sorted(matched_chars, key=lambda x: x['cx'])
+        plate_cx = (sorted_chars[0]['cx'] + sorted_chars[-1]['cx']) / 2
+        plate_cy = (sorted_chars[0]['cy'] + sorted_chars[-1]['cy']) / 2
+        plate_width = (sorted_chars[-1]['x'] + sorted_chars[-1]['w'] - sorted_chars[0]['x']) * PLATE_WIDTH_PADDING
+
+        sum_height = 0
+        for d in sorted_chars:
+            sum_height += d['h']
+        plate_height = int(sum_height / len(sorted_chars) * PLATE_HEIGHT_PADDING)
+        # 번호판의 높이와 빗변을 구해서, 기울어진 각도를 계산 
+        triangle_height = sorted_chars[-1]['cy'] - sorted_chars[0]['cy']
+        triangle_hypotenus = np.linalg.norm(
+            np.array([sorted_chars[0]['cx'], sorted_chars[0]['cy']]) - 
+            np.array([sorted_chars[-1]['cx'], sorted_chars[-1]['cy']]) 
+        )
+        angle = np.degrees(np.arctan(triangle_height / triangle_hypotenus))
+        rotation_matrix = cv2.getRotationMatrix2D(center=(plate_cx, plate_cy), angle=angle, scale=1)
+
+        img_rotated = cv2.warpAffine(img_threshed3, M=rotation_matrix, dsize=(WIDTH, HEIGHT))
+        img_cropped = cv2.getRectSubPix(
+            img_rotated,
+            patchSize=(int(plate_width), int(plate_height)),
+            center=(int(plate_cx), int(plate_cy))
+        )
+
+        if img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO \
+            or img_cropped.shape[1] / img_cropped.shape[0] < MIN_PLATE_RATIO > MAX_PLATE_RATIO:
+            continue
+
+        plate_imgs.append(img_cropped)
+        plate_infos.append({
+            'x' : int(plate_cx - plate_width / 2),
+            'y' : int(plate_cy - plate_height / 2),
+            'w' : int(plate_width),
+            'h' : plate_height
+        })
+        image_to_show.append(img_cropped)
+
+
 
     for i, img in enumerate(image_to_show):
         add_subplot_image(fig, img, '', i+1)
@@ -112,6 +254,8 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     print(cwd)
     print(sys.argv[0])
-    for img_file in os.listdir(cwd + IMAGE_FILE_PATH):
-        do_thing(cwd + IMAGE_FILE_PATH + img_file)
-        # break   # for test only one file
+    do_thing(cwd + IMAGE_FILE_PATH + '34.jpg')
+    do_thing(cwd + IMAGE_FILE_PATH + '65.jpg')
+    # for img_file in os.listdir(cwd + IMAGE_FILE_PATH):
+    #     do_thing(cwd + IMAGE_FILE_PATH + img_file)
+    #     break   # for test only one file
